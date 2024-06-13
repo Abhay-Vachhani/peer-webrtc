c6ece363688118a925177d9492eb52b532061f60
Abhay Vachhani <abhayvachhani28@gmail.com>
2024-06-13 08:45:57 +0530

    Created npm package peer-webrtc@1.0.1

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# peer-webrtc
+
+[![npm version](https://badge.fury.io/js/peer-webrtc.svg)](https://badge.fury.io/js/peer-webrtc)
+[![GitHub license](https://img.shields.io/github/license/Abhay-Vachhani/peer-webrtc.svg)](https://github.com/Abhay-Vachhani/peer-webrtc/blob/master/License)
+
+`peer-webrtc` simplifies the implementation of WebRTC connections by providing a straightforward API for establishing peer-to-peer communication. It's ideal for developers looking to integrate real-time audio, video, and data channels into their applications with minimal setup.
+
+## Features
+
+- Easy setup for WebRTC peer-to-peer connections.
+- Supports real-time audio, video, and data channels.
+- Minimalistic API for quick integration into applications.
+
+## Installation
+
+You can install `peer-webrtc` using npm:
+
+```bash
+npm install peer-webrtc
---
+++ index.js
@@ -0,0 +1,109 @@
+class PeerWebRTC {
+	constructor(initiator = false, stream, config = {}) {
+		this.peer = new RTCPeerConnection(config)
+		this.stream = stream
+		this.initiator = initiator
+		this.events = {
+			onSignal: () => {
+				console.warn(`onSignal called without a callback at ${new Date().toISOString()}`);
+			},
+			onConnect: () => {
+			},
+			addIceCandidate: () => {
+			},
+			onData: () => {
+			},
+			onStream: () => {
+			},
+		}
+
+		this.peer.ontrack = async (e) => {
+			if (e.streams.length > 0) {
+				await this.events.onStream(e.streams[0])
+			}
+		}
+
+		this.initialize()
+
+		this.peer.onconnectionstatechange = (e) => {
+			switch (this.peer.connectionState) {
+				case 'connected':
+					break;
+				default:
+					break;
+			}
+		}
+
+		this.peer.onicecandidate = (e) => {
+			if (e.candidate)
+				this.events.addIceCandidate(e.candidate)
+		}
+
+		this.peer.ondatachannel = (e) => {
+			const receivedDataChannel = e.channel;
+
+			receivedDataChannel.onmessage = (event) => {
+				this.events.onData(event.data)
+			};
+		}
+
+	}
+
+	async initialize() {
+		this.stream?.getTracks().forEach(track => {
+			this.peer.addTrack(track, this.stream)
+		})
+
+
+		this.datachannel = this.peer.createDataChannel('data')
+		this.datachannel.onopen = async () => {
+			await this.events.onConnect()
+		}
+
+		if (!this.initiator)
+			return
+
+		const offer = await this.peer.createOffer()
+		this.peer.setLocalDescription(offer)
+		await this.events.onSignal(offer)
+	}
+
+	async onSignal(callback) {
+		this.events.onSignal = callback
+	}
+
+	async addIceCandidate(callback) {
+		this.events.addIceCandidate = callback
+	}
+
+	async addICECandidate(candidate) {
+		this.peer.addIceCandidate(new RTCIceCandidate(candidate))
+	}
+
+	async onConnect(callback) {
+		this.events.onConnect = callback
+	}
+
+	async onData(callback) {
+		this.events.onData = callback
+	}
+
+	async onStream(callback) {
+		this.events.onStream = callback
+	}
+
+	async signal(sdp) {
+		await this.peer.setRemoteDescription(sdp)
+		if (this.initiator)
+			return
+		const answer = await this.peer.createAnswer()
+		this.peer.setLocalDescription(answer)
+		await this.events.onSignal(answer)
+	}
+
+	async send(data) {
+		this.datachannel.send(data)
+	}
+}
+
+export default PeerWebRTC
